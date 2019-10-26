632a3ea72b494f386d4b325ab2d8f005eaaa6869
rs <g@g.com>
2019-10-26 17:57:04 +0300

    Performed code on ColdObservables

--- src/ColdObservables.java
+++ src/ColdObservables.java
@@ -1,2 +1,18 @@
+import io.reactivex.Observable;
+
+/**
+ * Cold Observables replay all emissions to every Observer subscribed to them
+ */
 public class ColdObservables {
+    public static void main(String[] args) {
+        Observable<String> sizes = Observable.just("Small", "Medium", "Large", "Extra Large");
+
+        // first observer
+        sizes.subscribe(s -> System.out.println("Observer 1 received : " + s));
+
+        System.out.println("*** Second Observable ***");
+
+        // second observer
+        sizes.subscribe(t -> System.out.println("Observer 2 received : " + t));
+    }
 }
